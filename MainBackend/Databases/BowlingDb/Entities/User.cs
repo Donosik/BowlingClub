657de0e9caf3b4021c8a956f4a848a422fb83e11
Damian <damian1003@onet.pl>
2023-07-11 01:50:48 +0200

    User entity

---
+++ MainBackend/Databases/BowlingDb/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MainBackend.Databases.BowlingDb.Entities;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+    public int Login { get; set; }
+}
