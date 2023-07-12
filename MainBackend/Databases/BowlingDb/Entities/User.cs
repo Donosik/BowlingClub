00a8975faebbad0b210b10e239c51b52d7dbd088
Damian <damian1003@onet.pl>
2023-07-12 18:31:56 +0200

    Added regions for Entity

--- MainBackend/Databases/BowlingDb/Entities/User.cs
+++ MainBackend/Databases/BowlingDb/Entities/User.cs
@@ -4,7 +4,25 @@ namespace MainBackend.Databases.BowlingDb.Entities;
 
 public class User : IEntity
 {
+    #region Id
+
     [Key]
     public int Id { get; set; }
+
+    #endregion
+
+    #region Variables
+
     public int Login { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public User()
+    {
+        Login = 0;
+    }
+
+    #endregion
 }
