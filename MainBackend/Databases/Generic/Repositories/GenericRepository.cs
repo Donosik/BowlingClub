2e4f655101b75db5c90da59e18f9fb519c18fc89
Damian <damian1003@onet.pl>
2023-07-12 18:29:01 +0200

    Added regions for generic repository

--- MainBackend/Databases/Generic/Repositories/GenericRepository.cs
+++ MainBackend/Databases/Generic/Repositories/GenericRepository.cs
@@ -5,37 +5,64 @@ namespace MainBackend.Databases.BowlingDb.Repositories.Interfaces;
 
 public abstract class GenericRepository<T> : IGenericRepository<T> where T : class, IEntity
 {
+    #region Variables
+
     protected readonly DbContext dbContext;
+
+    #endregion
     
+
+    #region Constructors
+
     protected GenericRepository(DbContext dbContext)
     {
         this.dbContext = dbContext;
     }
+
+    #endregion
 
     public IQueryable<T> GetQuery()
     {
         return dbContext.Set<T>();
     }
 
+    #region Get
+
     public async Task<T> Get(int id)
     {
-        return await GetQuery().FirstOrDefaultAsync(t=>t.Id==id);
+        return await GetQuery().FirstOrDefaultAsync(t => t.Id == id);
     }
+
+    #endregion
+
+    #region GetAll
 
     public async Task<IEnumerable<T>> GetAll()
     {
         return await GetQuery().ToListAsync();
     }
 
+    #endregion
+
+    #region Create
+
     public void Create(T entity)
     {
         dbContext.Set<T>().Add(entity);
     }
 
+    #endregion
+
+    #region Edit
+
     public void Edit(T entity)
     {
         dbContext.Set<T>().Update(entity);
     }
+
+    #endregion
+
+    #region Delete
 
     public void Delete(T entity)
     {
@@ -47,4 +74,6 @@ public abstract class GenericRepository<T> : IGenericRepository<T> where T : cla
         T entity = await Get(id);
         Delete(entity);
     }
+
+    #endregion
 }
--- MainBackend/Databases/Generic/Repositories/IGenericRepository.cs
+++ MainBackend/Databases/Generic/Repositories/IGenericRepository.cs
@@ -5,14 +5,35 @@ namespace MainBackend.Databases.BowlingDb.Repositories.Interfaces;
 public interface IGenericRepository<T> where T : class, IEntity
 {
     IQueryable<T> GetQuery();
+
+    #region Get
+
     Task<T> Get(int id);
+
+    #endregion
+
+    #region GetAll
 
     Task<IEnumerable<T>> GetAll();
 
+    #endregion
+
+    #region Create
+
     void Create(T entity);
+
+    #endregion
+
+    #region Edit
 
     void Edit(T entity);
 
+    #endregion
+
+    #region Delete
+
     void Delete(T entity);
     Task Delete(int id);
+
+    #endregion
 }
