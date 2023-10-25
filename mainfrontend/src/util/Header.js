e5684ec0cb7cba7a7377e260b8ac0f3ec46086c0
Agnieszka <aga.g6664@gmail.com>
2023-10-25 17:59:02 +0200

    Header

--- mainfrontend/src/util/Header.js
+++ mainfrontend/src/util/Header.js
@@ -1,8 +1,35 @@
-export default function Header()
-{
-    return(
-        <>
-            Header
-        </>
-    )
+import './Header.css'
+import React from 'react';
+
+export default function Header() {
+    return (
+        <div className="container">
+            <div className='header'>
+                <div className='nagwek'>
+                    <div className='text'>
+                        <span>KRĘGIELNIA</span>
+                        <br />
+                        <span></span>
+                    </div>
+                    <span className='text05'>
+                        <span>BOWLING</span>
+                    </span>
+                </div>
+                <div className='menuugryzprzyciskami'>
+                    <a href={''} className='text07 navbar-toggler'>
+                        <span>LOGOWANIE</span>
+                    </a>
+                    <a href={''} className='text09'>
+                        <span>KONTAKT</span>
+                    </a>
+                    <a href={''} className='text11'>
+                        <span>OFERTA</span>
+                    </a>
+                    <a href={''} className='text13'>
+                        <span>HOME</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 }
