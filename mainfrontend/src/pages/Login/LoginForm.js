d7b353fe2fde0d7ea018289fe7f10251421fbe61
Damian <damian1003@onet.pl>
2023-11-02 18:07:41 +0100

    Changed error handling

--- mainfrontend/src/pages/Login/LoginForm.js
+++ mainfrontend/src/pages/Login/LoginForm.js
@@ -27,12 +27,15 @@ export default function LoginForm(props)
                 "password": password
             }
             const response = await axios.post(fetchAdress() + 'User/Login', requestData)
+
             setJWT(response.data)
         } catch (error)
         {
             setIsLoginFailed(true)
             //Jeśli fetche idą na zły endpoint to to powoduje error
-            setErrorMessage(error.response.data)
+            //setErrorMessage(error)
+            setErrorMessage("Błąd")
+            console.log(error)
         }
     }
 
@@ -46,28 +49,39 @@ export default function LoginForm(props)
     return (
         <>
             <div>
-            <form className="form-container">
-                <div>Login Page</div>
-                <label>
-                    Login:
-                    <input type="text" name="login" onChange={e => setLogin(e.target.value)} />
-                </label>
-                <label>
-                    Hasło:
-                    <input type="password" name="password" onChange={e => setPassword(e.target.value)} />
-                </label>
-                <label>Zapomniałeś hasła?</label>
-                <label>
-                    <input type="checkbox" name="rememberMe" />
-                    Zapamiętaj mnie
-                </label>
-                <button type="button" onClick={handleSubmit}>Zaloguj się</button><br/>
-                {isLoginFailed ? <div className="error-message">{errorMessage}</div> : null}
-                <img src="google_icon.png" alt="Google" onClick={handleSignUp} />
+                <form className="form-container">
+                    <div>Login Page</div>
+                    <label>
+                        Login:
+                        <input type="text"
+                               name="login"
+                               onChange={e => setLogin(e.target.value)}/>
+                    </label>
+                    <label>
+                        Hasło:
+                        <input type="password"
+                               name="password"
+                               onChange={e => setPassword(e.target.value)}/>
+                    </label>
+                    <label>Zapomniałeś hasła?</label>
+                    <label>
+                        <input type="checkbox"
+                               name="rememberMe"/>
+                        Zapamiętaj mnie
+                    </label>
+                    <button type="button"
+                            onClick={handleSubmit}>Zaloguj się
+                    </button>
+                    <br/>
+                    {isLoginFailed ? <div className="error-message">{errorMessage}</div> : null}
+                    <img src="google_icon.png"
+                         alt="Google"
+                         onClick={handleSignUp}/>
 
-                <button type="button">Google</button>
+                    <button type="button">Google</button>
 
-            </form></div>
+                </form>
+            </div>
         </>
     )
 }
