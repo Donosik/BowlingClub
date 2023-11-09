b080c45264e2bee6810533dc19d75fa23ca2b9cf
Agnieszka <aga.g6664@gmail.com>
2023-11-09 20:17:24 +0100

    zapomniany commit

--- mainfrontend/src/pages/Home/Home.js
+++ mainfrontend/src/pages/Home/Home.js
@@ -1,57 +1,53 @@
 import './home.css'
-import bowling_photo from './pobrany plik.jpg'
+import bowling_photo from './pobrany plik.jpeg'
 export default function Home()
 {
     return (
         <>
-            <div className="container justify-content-center">
-                <div className="row w-100 align-items-center">
-                    <div className="col-4 row box-1">
-
+            <div className="container d-flex w-100 justify-content-center align-content-center content-container">
+                <div className="d-flex align-items-center flex-row flex-wrap time-baba justify-content-between">
+                    <div className="box-1 flex-grow-1">
                         <p>GODZINY OTWARCIA</p>
 
-                        <p className="days">PONIEDZIAŁEK <span className="hours">16:00 - 22:00</span> </p>
-                        <p className="days">WTOREK<span className="hours">16:00 - 22:00</span> </p>
-                        <p className="days">ŚRODA <span className="hours">16:00 - 22:00</span></p>
-                        <p className="days">CZWARTEK <span className="hours">16:00 - 22:00</span></p>
-                        <p className="days">PIĄTEK <span className="hours">16:00 - 22:00</span></p>
-                        <p className="days">SOBOTA <span className="hours">16:00 - 22:00</span></p>
-                        <p className="days">NIEDZIELA<span className="hours">16:00 - 22:00</span></p>
-
-                    </div>
-                    <div className="col-8 justify-content-center box-2">
-                        <div className="city-card">
-                            <div className="activity-img">
-                                <img src={bowling_photo} alt="bowling"/>
-
-                            </div>
+                        <div className="date-time-container d-flex flex-column justify-content-between">
+                            <div className="date-time d-flex flex-row justify-content-between"><span>PONIEDZIAŁEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>WTOREK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>ŚRODA</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>CZWARTEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>PIĄTEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>SOBOTA</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>NIEDZIELA</span><span>16:00 - 22:00</span></div>
                         </div>
                     </div>
-                </div>
-
-                <div className="row w-100 align-items-center">
-                    <div className="col-8 justify-content-center box-2">
-                        <div className="city-card">
-                            <div className="activity-img">
-                                <img src={bowling_photo} alt="bowling"/>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-4 row box-1">
-
+                    <div className="box-1 flex-grow-1">
                         <p>GODZINY OTWARCIA</p>
 
-                        <p className="days">PONIEDZIAŁEK <p className="hours">16:00 - 22:00</p> </p>
-                        <p className="days">WTOREK<p className="hours">16:00 - 22:00</p> </p>
-                        <p className="days">ŚRODA <p className="hours">16:00 - 22:00</p></p>
-                        <p className="days">CZWARTEK <p className="hours">16:00 - 22:00</p></p>
-                        <p className="days">PIĄTEK <p className="hours">16:00 - 22:00</p></p>
-                        <p className="days">SOBOTA <p className="hours">16:00 - 22:00</p></p>
-                        <p className="days">NIEDZIELA<p className="hours">16:00 - 22:00</p></p>
-
+                        <div className="date-time-container d-flex flex-column justify-content-between">
+                            <div className="date-time d-flex flex-row justify-content-between"><span>PONIEDZIAŁEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>WTOREK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>ŚRODA</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>CZWARTEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>PIĄTEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>SOBOTA</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>NIEDZIELA</span><span>16:00 - 22:00</span></div>
+                        </div>
                     </div>
+                    <div className="box-2 baba-container flex-grow-1 d-flex">
+                        <img className="baba flex-grow-1 w-100" src={bowling_photo} alt="bowling"/>
+                    </div>
+                    <div className="box-1 w-100 align-self-stretch">
+                        <p>GODZINY OTWARCIA</p>
 
+                        <div className="date-time-container d-flex flex-column justify-content-between">
+                            <div className="date-time d-flex flex-row justify-content-between"><span>PONIEDZIAŁEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>WTOREK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>ŚRODA</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>CZWARTEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>PIĄTEK</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>SOBOTA</span><span>16:00 - 22:00</span></div>
+                            <div className="date-time d-flex flex-row justify-content-between"><span>NIEDZIELA</span><span>16:00 - 22:00</span></div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
