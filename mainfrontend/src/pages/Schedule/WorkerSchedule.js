98975026b02b29e84a586894bf52509f22a1f804
Agnieszka <aga.g6664@gmail.com>
2023-12-01 22:48:24 +0100

    tymczasowy kalendarz

--- mainfrontend/src/pages/Schedule/WorkerSchedule.js
+++ mainfrontend/src/pages/Schedule/WorkerSchedule.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './WorkerSchedule.css';
 
 export default function WorkerSchedule({ workersSchedule }) {
     const [selectedWorker, setSelectedWorker] = useState(null);
@@ -33,22 +34,20 @@ export default function WorkerSchedule({ workersSchedule }) {
     return (
         <div className="table-container">
             <div className="table-name">GRAFIK PRACOWNIKÓW</div>
-            <div className="table-container">
-              
-            </div>
+
+
+
             <div className="calendar-container">
+
                 <Calendar
                     value={selectedDate || new Date()}
                     onChange={handleCalendarChange}
-                    style={{
-                        backgroundColor: '#E6D1C0',
-                        color: '#591914',
-                    }}
-                    calendarType="US"
+
                 />
                 {selectedDate && (
                     <div>
-                        <h2>Pracownicy w dniu {selectedDate.toLocaleDateString()}</h2>
+                        <div className="table-name">PRACOWNICY W DNIU {selectedDate.toLocaleDateString()}</div>
+                        <div className="table-container">
                         <table className="table-bordered">
                             <thead>
                             <tr>
@@ -81,7 +80,7 @@ export default function WorkerSchedule({ workersSchedule }) {
                             ))}
                             </tbody>
                         </table>
-                    </div>
+                    </div> </div>
                 )}
                 <button onClick={handleCloseCalendar}>Zamknij kalendarz</button>
             </div>
