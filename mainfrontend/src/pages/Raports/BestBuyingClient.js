d9b9febdc8f44c40b94aa719a4e138f773fc59fc
Agnieszka <aga.g6664@gmail.com>
2023-12-01 20:18:28 +0100

    wykresy test

--- mainfrontend/src/pages/Raports/BestBuyingClient.js
+++ mainfrontend/src/pages/Raports/BestBuyingClient.js
@@ -1,31 +1,73 @@
 import React from "react";
+import Chart from "react-apexcharts";
 
-export default function BestBuyingClient({buyingClients})
-{
-    return(
+export default function BestBuyingClient({ buyingClients }) {
+    // Przygotuj dane do wykresu
+    const chartOptions = {
+        chart: {
+            id: "bar-chart",
+            background: "#E6D1C0",
+        },
+        xaxis: {
+            categories: buyingClients.map((client) => client.FullName),
+        },
+        plotOptions: {
+            bar: {
+                colors: {
+                    ranges: [
+                        {
+                            from: 0,
+                            to: 100,
+                            color: "#591914", // Zmień kolor słupków na #4CAF50
+                        },
+                    ],
+                },
+            },
+        },
+    };
+
+    const chartSeries = [
+        {
+            name: "Liczba faktur",
+            data: buyingClients.map((client) => client.Invoices),
+        },
+    ];
+
+    return (
         <div className="table-container">
-            <div className="table-name">
-                Najlepsi klienci</div>
+            <div className="table-name">Najlepsi klienci</div>
             <div className="table-container">
                 <table className="table-bordered">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Imię i nazwisko</th>
-                <th>Email</th>
-                <th>Liczba faktur</th>
-            </tr>
-            </thead>
-            <tbody>
-            {buyingClients.map((client)=>(
-                <tr key={client.Id}>
-                    <td>{client.Id}</td>
-                    <td>{client.FullName}</td>
-                    <td>{client.Email}</td>
-                    <td>{client.Invoices}</td>
-                </tr>
-            ))}
-            </tbody>
-            </table></div></div>
-    )
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Imię i nazwisko</th>
+                        <th>Email</th>
+                        <th>Liczba faktur</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {buyingClients.map((client) => (
+                        <tr key={client.Id}>
+                            <td>{client.Id}</td>
+                            <td>{client.FullName}</td>
+                            <td>{client.Email}</td>
+                            <td>{client.Invoices}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
+            {/* Dodaj wykres pod tabelą */}
+            <div className="chart-container">
+                <Chart
+                    options={chartOptions}
+                    series={chartSeries}
+                    type="bar"
+                    height={350}
+                />
+            </div>
+        </div>
+    );
 }
--- mainfrontend/src/pages/Raports/BestSellingProduct.js
+++ mainfrontend/src/pages/Raports/BestSellingProduct.js
@@ -1,29 +1,66 @@
 import React from "react";
+import Chart from "react-apexcharts";
 
-export default function BestSellingProduct({sellingProducts})
-{
-    return(
+export default function BestSellingProduct({ sellingProducts }) {
+    // Przygotuj dane do wykresu
+    const chartOptions = {
+        chart: {
+            id: "bar-chart",
+            background: "#E6D1C0",
+        },
+        xaxis: {
+            categories: sellingProducts.map((product) => product.ProductName),
+        },
+        plotOptions: {
+            bar: {
+                columnWidth: "50%", // Aby uzyskać efekt kolorowania słupków
+                distributed: true,
+            },
+        },
+        colors: ["#591914"], // Kolor słupków
+    };
+
+    const chartSeries = [
+        {
+            name: "Ilość sprzedanych sztuk",
+            data: sellingProducts.map((product) => product.Sold),
+        },
+    ];
+
+    return (
         <div className="table-container">
-            <div className="table-name">
-                Najlepiej sprzedające się produkty</div>
+            <div className="table-name">Najlepiej sprzedające się produkty</div>
             <div className="table-container">
                 <table className="table-bordered">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nazwa produktu</th>
-                <th>Ilość sprzedanych sztuk</th>
-            </tr>
-            </thead>
-            <tbody>
-            {sellingProducts.map((product)=>(
-                <tr key={product.Id}>
-                    <td>{product.Id}</td>
-                    <td>{product.ProductName}</td>
-                    <td>{product.Sold}</td>
-                </tr>
-            ))}
-            </tbody>
-                </table></div></div>
-    )
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nazwa produktu</th>
+                        <th>Ilość sprzedanych sztuk</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {sellingProducts.map((product) => (
+                        <tr key={product.Id}>
+                            <td>{product.Id}</td>
+                            <td>{product.ProductName}</td>
+                            <td>{product.Sold}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
+
+            {/* Dodaj wykres pod tabelą */}
+            <div className="chart-container">
+                <Chart
+                    options={chartOptions}
+                    series={chartSeries}
+                    type="bar"
+                    height={350}
+                />
+            </div>
+        </div>
+    );
 }
+
